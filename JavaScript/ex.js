29b6692d5c86d2c5a6f02ba52a5d90b244c437f4
unknown <ferchito.285.fg@gmail.com>
2024-02-19 19:55:29 +0100

    19hoy

---
+++ JavaScript/ex.js
@@ -0,0 +1,10 @@
+let hola = 2;
+console.log(hola);
+
+let txt = 'comando';
+console.log(txt);
+
+let a;
+a = 7;
+let b = a;
+console.log(b)
---
+++ JavaScript/ex2.js
@@ -0,0 +1,7 @@
+let a = 5;
+let b = 7;
+let c = a;
+a = b;
+b = c;
+console.log('a: ', a);
+console.log('b: ', b);
